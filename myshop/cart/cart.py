1c5f3000a92f413eb87298662fb740d2e8266f04
diekaltesonne <diekaltesonne2000@mail.ru>
2021-03-02 21:55:56 +0300

    update 03.02.21 09:55

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -29,4 +29,44 @@ class Cart(object):
         # Помечаем сессию как измененную
         self.session.modified = True
 
+    def remove(self, product):
+        ''' Удаление товара из корзины '''
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def __iter__(self):
+        ''' Проходим по товарам корзины и получаем соотвествующие объекты Product. '''
+        product_ids = self.cart.keys()
+        # Получаем объекты модели Product и передаем их в корзину.
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product_ids)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+        yield item
+    
+    def __len__(self):
+        ''' Возвращает общее количество товаров в корзине '''
+        return sum (item['quantity'] for item in self.cart.values())
+    
+    def get_total_price(self):
+        return sum(
+            Decimal(item['price'] * item['quantity'])
+            for item in self.cart.values()
+        )
+    
+    def clear(self):
+        # Oчистка корзины.
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+
+        
+
 
